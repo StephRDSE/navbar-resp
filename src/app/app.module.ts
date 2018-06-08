32105924b31ee44b8714bf70fa281e278d7ae860
unknown <stephane.rodolausse.pf1@gmail.com>
2018-06-08 11:39:37 +0200

    commit du midi

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,52 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MyNavComponent } from './my-nav/my-nav.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule } from '@angular/material';
+import { RouterModule, Routes } from '@angular/router';
+import { BioComponent } from './bio/bio.component';
+import { RealisationsComponent } from './realisations/realisations.component';
+import { ContactComponent } from './contact/contact.component';
+
+const routes: Routes = [
+  {
+    path: 'bio',
+    component: BioComponent
+  },
+  {
+    path: 'contact',
+    component: ContactComponent
+  },
+  {
+    path: 'realisations',
+    component: RealisationsComponent
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyNavComponent,
+    BioComponent,
+    RealisationsComponent,
+    ContactComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
